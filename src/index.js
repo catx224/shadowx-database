45743bec108544f5a5d23bfff0711401877e17b2
MUEID MURSALIN RIFAT <catx224@gmail.com>
2026-07-22 18:34:09 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@
 
 const Database = require('./database');
 const { ShadowXError, ErrorCodes } = require('./errors');
+const { EventManager, EventTypes } = require('./events');
 
 module.exports = Database;
 module.exports.ShadowXError = ShadowXError;
 module.exports.ErrorCodes = ErrorCodes;
+module.exports.EventManager = EventManager;
+module.exports.EventTypes = EventTypes;
 module.exports.default = Database;
